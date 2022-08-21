ef526b2baabc925788af87a272874286a7c4eab6
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-08-21 15:22:42 +0300

    correction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,92 @@
 import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
+import API from 'API/API';
 import 'react-toastify/dist/ReactToastify.css';
 import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
 import { Loader } from './Loader';
+import { Modal } from './Modal';
+import { BtnLoadMore } from 'components/Button';
+
+const imgMapper = dataHits => {
+  return dataHits.map(({ id, webformatURL, largeImageURL }) => {
+    return { id, webformatURL, largeImageURL };
+  });
+};
 
 export class App extends Component {
   state = {
     query: '',
     loading: false,
+    images: [],
+    page: 1,
+    largeImg: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query } = this.state;
+    const { page } = this.state;
+    if (prevState.query !== query) {
+      this.setState({ page: 1 });
+      return this.getImages(true);
+    }
+
+    if (prevState.page !== page && page !== 1) {
+      return this.getImages(false);
+    }
+  }
+
+  getImages = newQuery => {
+    const { query } = this.state;
+    // const { page } = this.state;
+    const page = newQuery ? 1 : this.state.page;
+    this.setState({ loading: true });
+
+    API.fetchApi(query, page)
+      .then(data =>
+        this.setState(prevState => ({
+          images: newQuery
+            ? imgMapper(data.hits)
+            : [...prevState.images, ...imgMapper(data.hits)],
+        }))
+      )
+      .finally(() => this.setState({ loading: false }));
   };
 
   handleFormSubmit = query => {
     this.setState({ query });
   };
 
+  loadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  handleClickImg = largeImg => {
+    this.setState({ largeImg });
+  };
+
+  closeModal = () => {
+    this.setState({ largeImg: null });
+  };
+
   render() {
-    const { loading } = this.state;
+    const { loading, images, largeImg, query } = this.state;
     return (
       <div>
         {loading && <Loader />}
         <Searchbar onSubmit={this.handleFormSubmit} />
         <ToastContainer />
-        <ImageGallery query={this.state.query} />
+        <ImageGallery
+          query={query}
+          images={images}
+          onClick={this.handleClickImg}
+        />
+
+        {images.length > 0 && <BtnLoadMore onClick={this.loadMore} />}
+
+        {largeImg && (
+          <Modal largeImg={largeImg} query={query} onClose={this.closeModal} />
+        )}
       </div>
     );
   }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -12,7 +12,6 @@ import {
 export class Searchbar extends Component {
   state = {
     query: '',
-    images: [],
   };
 
   handleChange = e => {
